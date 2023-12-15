9cb2e1c98c206dfd2b0d8ce6f821e88c86ccde8a
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2023-12-15 22:16:59 +0200

    Create User Agreement Page

---
+++ src/Content/UserAgreement.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const UserAgreement = () => {
+  return (
+    <div>
+      <div className="max-w-[1280px] m-auto xs:mx-4 sm:mx-4 md:mx-4 lg:mx-4 xl:mx-4 xs:mt-60  sm:mt-60 md:mt-60">
+        <div className="text-center text-3xl font-bold my-8 text-[#213555]">
+          اتفاقية المستخدم
+        </div>
+        <div className="text-right">
+          <div className="text-[17px] leading-loose mb-8">
+            <br />
+            <span className="text-[20px] text-[#213555] font-bold">1. الموافقة:</span>
+            <br />
+            أ. بتسجيل الدخول واستخدام هذا الموقع، فإنك توافق على جميع شروط
+            وأحكام هذه الاتفاقية.
+            <br />
+            <span className="text-[20px] text-[#213555] font-bold">2. المحتوى:</span>
+            <br />
+            أ. المستخدم مسؤول عن المحتوى الذي يقوم بنشره على صفحة الفيسبوك. ب.
+            يجب أن يتوافق المحتوى مع قوانين حقوق الملكية الفكرية ولا ينتهك حقوق
+            الآخرين.
+            <br />
+            <span className="text-[20px] text-[#213555] font-bold">3. حقوق النشر:</span>
+            <br />
+            أ. المستخدم يمنح إذنًا غير حصرية لصاحب الموقع لاستخدام ونشر المحتوى
+            على صفحة الفيسبوك.
+            <br />
+            <span className="text-[20px] text-[#213555] font-bold">4. السلوك:</span>
+            <br />
+            أ. يجب على المستخدم الامتناع عن نشر محتوى غير لائق أو ينتهك قوانين
+            الفيسبوك. ب. يجب أن يلتزم المستخدم بسياسات الفيسبوك وقوانينها.
+            <br />
+            <span className="text-[20px] text-[#213555] font-bold">5. الخصوصية:</span>
+            <br />
+            أ. المعلومات الشخصية المقدمة قد تكون معرضة للعرض على صفحة الفيسبوك
+            وفقًا لسياسات الخصوصية.
+            <br />
+            <span className="text-[20px] text-[#213555] font-bold">6. المسؤولية:</span>
+            <br />
+            أ. لا تتحمل الشركة المسؤولية عن أي أضرار ناتجة عن استخدام أو عدم
+            قدرة على استخدام الموقع.
+            <br />
+            <span className="text-[20px] text-[#213555] font-bold">7. التعديلات:</span>
+            <br />
+            أ. يحق للشركة تعديل شروط الاتفاقية وإعلام المستخدمين بذلك.
+            <br />
+            <span className="text-[20px] text-[#213555] font-bold">8. إنهاء الخدمة:</span>
+            <br />
+            أ. يحق للشركة إنهاء حساب المستخدم أو صفحة الفيسبوك في حالة انتهاك أي
+            من بنود هذه الاتفاقية.
+            <br />
+            <span className="text-[20px] text-[#213555] font-bold">9. القانون الساري:</span>
+            <br />
+            أ. هذه الاتفاقية تخضع لقوانين الدولة/البلد الذي يدير فيه الموقع.
+            <br />
+            <span className="text-[20px] text-[#213555] font-bold">10. تصفية النزاع:</span>
+            <br />
+            أ. في حالة وجود نزاع، يتم التوصل إلى حل ودي في المقام الأول، وإلا
+            سيتم إحالة النزاع إلى القضاء.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserAgreement;
